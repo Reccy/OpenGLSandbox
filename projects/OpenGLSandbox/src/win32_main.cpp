df60d90876be74ec429809820c5b089785af1b9a
Aaron Meaney <aaronmeaney@hotmail.com>
2022-07-24 17:36:13 +0100

    Add glClear test program

--- projects/OpenGLSandbox/src/win32_main.cpp
+++ projects/OpenGLSandbox/src/win32_main.cpp
@@ -1,42 +1,83 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+struct Vector4
+{
+	float x, y, z, w;
+};
+
+static Vector4 Red { 1.0f, 0.0f, 0.0f, 1.0f };
+static Vector4 Green { 0.0f, 1.0f, 0.0f, 1.0f };
+static Vector4 Blue { 0.0f, 0.0f, 1.0f, 1.0f };
+static Vector4 White { 1.0f, 1.0f, 1.0f, 1.0f };
+
+static Vector4 *ClearColor = &Red;
+
+void GL_FramebufferResized(GLFWwindow* window, int width, int height)
+{
+	std::cout << "Framebuffer resized (" << width << "x, " << height << "y)" << std::endl;
+	glViewport(0, 0, width, height);
+}
+
+void GL_ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		ClearColor = &Red;
+
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		ClearColor = &Green;
+
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		ClearColor = &Blue;
+
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		ClearColor = &White;
+}
+
 int main(void)
 {
-    GLFWwindow* window;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 
-    /* Initialize GLAD */
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-        return -1;
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	glViewport(0, 0, 800, 600);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	glfwSetFramebufferSizeCallback(window, GL_FramebufferResized);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	while (!glfwWindowShouldClose(window))
+	{
+		GL_ProcessInput(window);
 
-    glfwTerminate();
-    return 0;
+		glClearColor(ClearColor->x, ClearColor->y, ClearColor->z, ClearColor->w);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
